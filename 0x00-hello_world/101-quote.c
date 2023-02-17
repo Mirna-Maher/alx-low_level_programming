9189e28a5a45f8b4f99d111be8ac177ffa7cee6f
Mirna Maher <Mirna@example.com>
2023-02-17 07:41:36 -0800

    100-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unist.h>
 /**
- * main - A program that prints a line
+ * main - A program that prints a line 
  * Return: 1 (Success)
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
