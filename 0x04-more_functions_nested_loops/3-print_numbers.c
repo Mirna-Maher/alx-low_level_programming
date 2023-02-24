38e286c9bd0ae53e1161165706fb9fc2247ede3e
Mirna Maher <Mirna@example.com>
2023-02-24 07:25:46 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that will print numbers
+ * i is the number
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i >= 0; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+}
